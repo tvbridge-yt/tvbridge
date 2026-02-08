7d601d13288784dfbcce9adf83c5d6f8be91f59d
tvbridge-yt <tvbridge-yt@users.noreply.github.com>
2026-02-08 09:20:47 +0900

    halo2 doc, partial

---
+++ src/tvbridge/core/__init__.py
@@ -0,0 +1,5 @@
+from tvbridge.core.episode import BroadcastEpisode
+from tvbridge.core.topic import ResearchTopic
+from tvbridge.core.schedule import WeeklySchedule
+
+__all__ = ["BroadcastEpisode", "ResearchTopic", "WeeklySchedule"]
